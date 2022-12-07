ff450def84a86b95b8aa8a59e509102071d7712c
Gabriel Vogel <gabriel.vogel@online.de>
2022-12-07 12:07:26 +0100

    Sixth day of Advent of Code 2022

---
+++ src/bin/day6-1.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use std::process::exit;
+
+static INPUT: &str = "input/day6.txt";
+const MARK_LEN: usize = 14;
+
+fn main() {
+    let input = fs::read_to_string(INPUT)
+        .unwrap_or_else(|e| {
+            eprintln!("Could not read input file: {}", e);
+            exit(1);
+        });
+    let bytes = input.as_bytes();
+    for i in MARK_LEN..bytes.len() {
+        if check_marker(&bytes[i-MARK_LEN..i]) {
+            println!("{}", i);
+            exit(0);
+        }
+    }
+    println!("No marker found!");
+}
+
+fn check_marker(marker: &[u8]) -> bool {
+    let len = marker.len();
+    for i in 0..len {
+        for j in i+1..len {
+            if marker[i] == marker[j] {
+                return false;
+            }
+        }
+    }
+    return true;
+}
