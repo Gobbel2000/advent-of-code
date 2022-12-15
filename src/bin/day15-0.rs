1ecda922318df3df28af4ee9344612a03260c09d
Gabriel Vogel <gabriel.vogel@online.de>
2022-12-15 15:02:34 +0100

    Fifteenth day of Advent of Code 2022

---
+++ src/bin/day15-0.rs
@@ -0,0 +1,106 @@
+use std::fs;
+use std::process::exit;
+use std::ops::Range;
+
+use regex::Regex;
+use lazy_static::lazy_static;
+
+static INPUT: &str = "input/day15.txt";
+const SLICE: i32 = 2_000_000;
+
+#[derive(Debug)]
+struct Sensor {
+    pos: (i32, i32),
+    beacon: (i32, i32),
+    dist: u32,
+}
+
+fn main() {
+    let input = fs::read_to_string(INPUT)
+        .unwrap_or_else(|e| {
+            eprintln!("Could not read input file: {e}");
+            exit(1);
+        });
+    let sensors = parse_input(&input);
+    let ranges = get_ranges(&sensors);
+    let condensed = condense_ranges(ranges);
+    let n_range: usize = condensed.iter()
+        .map(|r| r.len())
+        .sum();
+    println!("{}", n_range - beacons_on_slice(&sensors));
+}
+
+fn parse_input(input: &str) -> Vec<Sensor> {
+    lazy_static! {
+        static ref PAT: Regex = Regex::new(
+            r"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$")
+            .unwrap();
+    }
+    let mut sensors = Vec::new();
+    for line in input.lines() {
+        let caps = PAT.captures(line).unwrap_or_else(|| {
+            eprintln!("Malformed line: {}", line);
+            exit(5);
+        });
+        let pos = (caps.get(1).unwrap().as_str().parse().unwrap(),
+                   caps.get(2).unwrap().as_str().parse().unwrap());
+        let beacon = (caps.get(3).unwrap().as_str().parse().unwrap(),
+                      caps.get(4).unwrap().as_str().parse().unwrap());
+        let dist = manhattan(pos, beacon);
+        sensors.push(Sensor { pos, beacon, dist });
+    }
+    return sensors;
+}
+
+fn manhattan(p1: (i32, i32), p2: (i32, i32)) -> u32 {
+    p1.0.abs_diff(p2.0) + p1.1.abs_diff(p2.1)
+}
+
+fn get_ranges(sensors: &Vec<Sensor>) -> Vec<Range<i32>> {
+    let mut ranges: Vec<Range<i32>> = sensors.iter()
+        .filter_map(|s| {
+            let width = s.dist.checked_sub(s.pos.1.abs_diff(SLICE))? as i32;
+            Some((s.pos.0 - width)..(s.pos.0 + width + 1))
+        })
+        .collect();
+    ranges.sort_unstable_by_key(|r| r.start);
+    return ranges;
+}
+
+fn condense_ranges(ranges: Vec<Range<i32>>) -> Vec<Range<i32>> {
+    let mut condensed = Vec::new();
+    if ranges.len() == 0 {
+        return condensed;
+    }
+    let mut cur = ranges[0].clone();
+    for r in ranges[1..].iter() {
+        if r.start > cur.end + 1 {
+            condensed.push(cur);
+            cur = r.clone();
+        } else if cur.end < r.end {
+            cur.end = r.end;
+        }
+    }
+    condensed.push(cur);
+
+    return condensed;
+}
+
+// Number of unique beacons that lie on the observed slice
+fn beacons_on_slice(sensors: &Vec<Sensor>) -> usize {
+    let mut beacons: Vec<(i32, i32)> = sensors.iter()
+        .map(|s| s.beacon)
+        .filter(|b| b.1 == SLICE)
+        .collect();
+    if beacons.len() == 0 {
+        return 0;
+    }
+    beacons.sort();
+    let mut count = 1;
+    for i in 1..beacons.len() {
+        if beacons[i].0 != beacons[i-1].0 {
+            count += 1;
+        }
+    }
+    return count;
+}
